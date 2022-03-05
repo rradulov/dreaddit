8529709048ea4906c1cd54e7ea582d085672e988
Helene Lipp <ch.lipp@yahoo.com>
2022-03-05 17:07:16 +0000

    app.py first steps

--- app.py
+++ app.py
@@ -4,10 +4,20 @@ import numpy as np
 import pandas as pd
 
 st.title('Dreaddit Stress Analysis')
-st.markdown("""# This is a header
-## This is a sub header
-This is text""")
-st.text('This is an example')
+st.markdown("""# Stressed? Find it out by Social Media texts!""")
+st.text("""
+Stress is a nigh-universal human experience particularly in the online world.
+While stress can be a motivator, too much stress is associated with many
+negative health outcomes, making its identification useful across a range
+of domains. However, existing computational research typically only studies
+stress in domains such as speech, or in short genres such as Twitter.
+We present Dreaddit, a new text corpus of lengthy multi-domain social media
+data for the identification of stress. Our dataset consists of 190K posts
+from five different categories of Reddit communities; we additionally label
+3.5K total segments taken from 3K posts using Amazon Mechanical Turk.
+We present preliminary supervised learning methods for identifying stress,
+both neural and traditional, and analyze the complexity and diversity of the
+data and characteristics of each category""")
 placeholder = st.empty()
 
 txt = st.text_area('Text to analyze', '''
@@ -17,7 +27,23 @@ txt = st.text_area('Text to analyze', '''
      was the season of Light, it was the season of Darkness, it
      was the spring of hope, it was the winter of despair, (...)
      ''')
-st.write('Sentiment:', run_sentiment_analysis(txt))
+st.write('Stressfactor:', run_sentiment_analysis(txt))
+
+txt2 = st.text_area('Text to analyze', '''
+     It was the best of times, it was the worst of times, it was
+     the age of wisdom, it was the age of foolishness, it was
+     the epoch of belief, it was the epoch of incredulity, it
+     was the season of Light, it was the season of Darkness, it
+     was the spring of hope, it was the winter of despair, (...)
+     ''')
+
+txt3 = st.text_area('Text to analyze', '''
+     It was the best of times, it was the worst of times, it was
+     the age of wisdom, it was the age of foolishness, it was
+     the epoch of belief, it was the epoch of incredulity, it
+     was the season of Light, it was the season of Darkness, it
+     was the spring of hope, it was the winter of despair, (...)
+     ''')
 
 # this slider allows the user to select a number of lines
 # to display in the dataframe
